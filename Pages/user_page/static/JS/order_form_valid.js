e844cb2514e1489375eb95d459b15291b12b0b35
SapirLulvi <lulvi@post.bgu.ac.il>
2021-12-12 20:45:14 +0200

    Final version

--- Pages/user_page/static/JS/order_form_valid.js
+++ Pages/user_page/static/JS/order_form_valid.js
@@ -1,10 +1,4 @@
 
-function checkValidDate(orderD,note){
-    const inpObj = document.getElementById(orderD);
-    if (!inpObj.checkValidity()) {
-    document.getElementById(note).innerHTML = inpObj.validationMessage;
-    }
-}
 
 function checkValidPhone(phoneNum,note){
     const inpObj = document.getElementById(phoneNum);
